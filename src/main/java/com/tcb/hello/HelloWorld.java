a3bce65b3a6ebd5707b9fd83d9349e12b25deb9d
Mezlini_Ahmed <mezliniahmed13@gmail.com>
2018-01-19 10:43:29 +0100

    first commit

---
+++ src/main/java/com/tcb/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.tcb.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        Hello();
+    }
+    public static void Hello() {
+    	System.out.println( "Hello World!" );
+    }
+}
